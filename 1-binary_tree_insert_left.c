c3badba296886768da2dcaf23311e81bef3eca97
i1SaSa <s9ny398335@gmail.com>
2025-12-09 18:37:59 +0300

    Fix order of null check for parent in binary_tree_insert_left function

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,13 +11,14 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
 
+	if (!parent)
+		return (NULL);
 	node = malloc(sizeof(binary_tree_t));
 	if (!node)
 	{
 		return (NULL);
 	}
-	if (!parent)
-		return (NULL);
+
 	node->n = value;
 	node->parent = parent;
 	node->left = NULL;
