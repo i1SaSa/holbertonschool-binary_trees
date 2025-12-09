e07c47ffa4c081860056df6fb4ce5d610158f7cd
i1SaSa <s9ny398335@gmail.com>
2025-12-09 17:25:52 +0300

    Fix trailing whitespace

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,7 +3,7 @@
  * binary_tree_node - create a tree
  * @parent: pointer to the parent
  * @value: value of the node
- * 
+ *
  * Return: pointer to the root
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
