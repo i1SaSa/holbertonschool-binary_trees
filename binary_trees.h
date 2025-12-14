84c50b968381eb6ed45cd0391b3bd4bc3d7cbca4
rabea14180-max <rabea1418@gmail.com>
2025-12-14 17:08:07 +0000

    add  function Sibling

--- binary_trees.h
+++ binary_trees.h
@@ -35,7 +35,7 @@ int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
-
+binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 
 
 #endif /* BINARY_TREES */
