1576026c1671b4a3d1e2a141ba7a2bc82f9fb3b9
rabea14180-max <rabea14180@gmail.com>
2025-12-15 11:07:35 +0000

    add  a function Balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -4,21 +4,27 @@
 /**
  * binary_tree_height - measures the height of a binary tree
  * @tree: pointer to the root node
- * Return: height of the tree, 0 if NULL
+ *
+ * Return: height of the tree
+ *         0 if tree is NULL
  */
 int binary_tree_height(const binary_tree_t *tree)
 {
 int left_height, right_height;
 if (!tree)
 return (0);
-left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-return (left_height > right_height ? left_height : right_height);
+left_height = binary_tree_height(tree->left);
+right_height = binary_tree_height(tree->right);
+if (left_height > right_height)
+return (left_height + 1);
+return (right_height + 1);
 }
 /**
  * binary_tree_balance - measures the balance factor of a node
  * @tree: pointer to the node
- * Return: balance factor (left - right), 0 if NULL
+ *
+ * Return: balance factor (left subtree height - right subtree height)
+ *         0 if tree is NULL
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
